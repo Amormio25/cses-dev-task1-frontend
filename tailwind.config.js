5a6e27cc37ad172842280324f9b0210a9bf46ee8
Amormio Velasquez III <amorbuks25@gmail.com>
2025-11-26 13:20:10 -0800

    Created a notes text file following instructions to use tailwind v3 inside webpack template

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // gets all html and js files in src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
